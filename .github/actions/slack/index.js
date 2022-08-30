3da8d7380d195780f57ef7b29ded5e9cc6ea6d3f
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2022-08-30 18:05:50 +0200

    Test slack 400 error

--- .github/actions/slack/index.js
+++ .github/actions/slack/index.js
@@ -1,42 +1,53 @@
-const core = require("@actions/core");
-const axios = require("axios");
+/**
+ * If you are updating this file you have to rebuild dist/index.js
+ * Run following commands to create a new build:
+ *   meteor npm install --no-save @actions/core @actions/github @vercel/ncc axios slackify-markdown
+ *   cd .github/actions/slack
+ *   meteor npx ncc build ./index.js
+ * Include rebuilt dist/index.js in the commit
+ *
+ * -- details on this requirement: https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github
+ */
+
+const core = require('@actions/core');
+const axios = require('axios');
 const slackifyMarkdown = require('slackify-markdown');
 
 async function run() {
   try {
     const slackWebhook = process.env.SLACK_WEBHOOK;
-    const changelogMessage = core.getInput("CHANGELOG_MESSAGE");
-    const version = core.getInput("VERSION");
-    const releaseUrl = core.getInput("RELEASE_URL");
+    const changelogMessage = core.getInput('CHANGELOG_MESSAGE');
+    const version = core.getInput('VERSION');
+    const releaseUrl = core.getInput('RELEASE_URL');
 
     const payload = {
       blocks: [
         {
-          type: "header",
+          type: 'header',
           text: {
-            type: "plain_text",
+            type: 'plain_text',
             emoji: true,
             text: `New release v${version} :rocket:`,
           },
         },
         {
-          type: "section",
+          type: 'section',
           text: {
-            type: "mrkdwn",
-            text: `@channel <${releaseUrl}>`,
+            type: 'mrkdwn',
+            text: `Hey Maestros! See what brings in a new release :magic_wand: \n<${releaseUrl}>`,
           },
         },
         {
-          type: "header",
+          type: 'header',
           text: {
-            type: "plain_text",
-            text: "Changelog",
+            type: 'plain_text',
+            text: 'Changelog',
           },
         },
         {
-          type: "section",
+          type: 'section',
           text: {
-            type: "mrkdwn",
+            type: 'mrkdwn',
             text: slackifyMarkdown(changelogMessage),
           },
         },
@@ -50,3 +61,4 @@ async function run() {
 }
 
 run();
+ 
