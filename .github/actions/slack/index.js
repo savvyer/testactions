eb44892dde550a08b66d1871fad3d8fb96f3656f
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2022-09-01 12:09:52 +0200

    Refactor changelog

--- .github/actions/slack/index.js
+++ .github/actions/slack/index.js
@@ -1,7 +1,7 @@
 /**
  * If you are updating this file you have to rebuild dist/index.js
  * Run following commands to create a new build:
- *   meteor npm install --no-save @actions/core @actions/github @vercel/ncc axios slackify-markdown
+ *   meteor npm install --no-save @actions/core @actions/github @vercel/ncc axios
  *   cd .github/actions/slack
  *   meteor npx ncc build ./index.js
  * Include rebuilt dist/index.js in the commit
@@ -11,48 +11,83 @@
 
 const core = require('@actions/core');
 const axios = require('axios');
-const slackifyMarkdown = require('slackify-markdown');
+
+function createSlackTextSection(text) {
+  return {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text,
+    },
+  };
+}
+
+function createSlackPayload({ version, releaseUrl, shortcutLinks, changelog }) {
+  const payload = {
+    blocks: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          emoji: true,
+          text: `New release v${version} :rocket:`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey Maestros! See what brings in a new release :magic_wand: \n<${releaseUrl}>`,
+        },
+      },
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: 'Released stories',
+        },
+      },
+    ],
+  };
+
+  if (shortcutLinks.length) {
+    shortcutLinks.forEach(link => {
+      const textSection = createSlackTextSection(`• ${link}`);
+      payload.blocks.push(textSection);
+    });
+  } else {
+    const textSection = createSlackTextSection('None 🤷‍♀️ 🤷‍♂️');
+    payload.blocks.push(textSection);
+  }
+
+  payload.blocks.push({
+    type: 'header',
+    text: {
+      type: 'plain_text',
+      text: 'Changelog',
+    },
+  });
+
+  if (changelog.length) {
+    changelog.forEach(logEntry => {
+      const textSection = createSlackTextSection(`• ${logEntry}`);
+      payload.blocks.push(textSection);
+    });
+  } else {
+    const textSection = createSlackTextSection('None 🤷‍♂️ 🤷‍♀️');
+    payload.blocks.push(textSection);
+  }
+}
 
 async function run() {
   try {
     const slackWebhook = process.env.SLACK_WEBHOOK;
-    const changelogMessage = core.getInput('CHANGELOG_MESSAGE');
+    const shortcutLinks = JSON.parse(core.getInput('SHORTCUT_LINKS'));
+    const changelog = JSON.parse(core.getInput('CHANGELOG'));
     const version = core.getInput('VERSION');
     const releaseUrl = core.getInput('RELEASE_URL');
 
-    const payload = {
-      blocks: [
-        {
-          type: 'header',
-          text: {
-            type: 'plain_text',
-            emoji: true,
-            text: `New release v${version} :rocket:`,
-          },
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `Hey Maestros! See what brings in a new release :magic_wand: \n<${releaseUrl}>`,
-          },
-        },
-        {
-          type: 'header',
-          text: {
-            type: 'plain_text',
-            text: 'Changelog',
-          },
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: slackifyMarkdown(changelogMessage),
-          },
-        },
-      ],
-    };
+    const payload = createSlackPayload({ version, releaseUrl, shortcutLinks, changelog });
 
     await axios.post(slackWebhook, payload);
   } catch (error) {
